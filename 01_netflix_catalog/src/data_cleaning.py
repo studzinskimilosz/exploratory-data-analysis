1608641f80e8ae15f3b432abdb612642c2e44f52
Milosz Studzinski <studzinskimilosz@gmail.com>
2026-07-22 13:50:57 +0200

    feat: finish data cleaning

--- 01_netflix_catalog/src/data_cleaning.py
+++ 01_netflix_catalog/src/data_cleaning.py
@@ -25,7 +25,7 @@ print(df.duplicated().sum()) #no duplicates
 ### Text columns
 print(df.columns)
 
-text_columns = ['type', 'type', 'title', 'director', 'cast', 'country', 'rating', 'duration', 'listed_in', 'description']
+text_columns = ['type', 'title', 'director', 'cast', 'country', 'rating', 'duration', 'listed_in', 'description']
 
 for col in text_columns:
     df[col] = df[col].str.strip().str.lower().str.replace(' ', '_')
@@ -35,25 +35,23 @@ df['date_added'] = pd.to_datetime(df['date_added'], errors='coerce')
 
 df["year_added"] = df["date_added"].dt.year
 df["month_added"] = df["date_added"].dt.month
-df["month_name_added"] = df["date_added"].dt.month_name()
+df["month_name_added"] = df["date_added"].dt.month_name().str.strip().str.lower()
 
 df['age_of_show'] = df['year_added'] - df['release_year']
 
-df['age_of_show'].describe()
-
-### Negative values
-col_to_check = ['title', 'release_year', 'date_added', 'year_added', 'age_of_show']
-
-negavite_check = df.loc[df['age_of_show'] < 0, col_to_check]
-
-## Handling this values
 df.loc[df['age_of_show'] < 0, 'age_of_show'] = np.nan
+
+df['age_of_show'].describe()
 
 ### Time handling
 df['duration_value'] = df['duration'].str.extract(r'(\d+)', expand=False)
 df['duration_value'] = pd.to_numeric(df['duration_value'], errors='coerce')
 
+df.loc[df['duration_value'] <= 0, 'duration_value'] = np.nan
+
 # print(df['type'].unique())
 df['movie_duration_min'] = np.where(df['type'] =='movie', df['duration_value'], np.nan)
-df['tv_seasons'] = np.where(df['type'] =='tv_Show', df['duration_value'], np.nan)
+df['tv_seasons'] = np.where(df['type'] =='tv_show', df['duration_value'], np.nan)
 
+### save csv
+df.to_csv("../data/clean_netflix_titles.csv", index=False)
