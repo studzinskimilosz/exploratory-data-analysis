800ec0c6b7ac57782041a1daf9b002ad3e0560ca
Milosz Studzinski <studzinskimilosz@gmail.com>
2026-07-22 12:13:51 +0200

    feat: Start cleaning data

---
+++ 01_netflix_catalog/src/data_cleaning.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("../data/netflix_titles.csv")
+
+### Description
+print(df.info())
+print(df.describe())
+
+### Missing values
+na_values = df.isnull().sum()
+percentage_na = na_values * 100 / len(df)
+
+missing_values = {
+    'Missing numbers': na_values.sort_values(ascending=False),
+    'Missing percentage': percentage_na.sort_values(ascending=False)
+}
+
+print(missing_values)
+
+### Duplicates
+
+print(df.duplicated().sum()) #no duplicates
+
+### Text columns
+print(df.columns)
+
+text_columns = ['type', 'title', 'director', 'cast', 'country', 'rating', 'duration', 'listed_in', 'description']
+
+for col in text_columns:
+    df[col] = df[col].str.strip()
+
+### Date
+df['date_added'] = pd.to_datetime(df['date_added'], errors='coerce')
+
+df["year_added"] = df["date_added"].dt.year
+df["month_added"] = df["date_added"].dt.month
+df["month_name_added"] = df["date_added"].dt.month_name()
+
+df['age_of_show'] = df['year_added'] - df['release_year']
+
+df['age_of_show'].describe()
+
+### Negative values
+col_to_check = ['title', 'release_year', 'date_added', 'year_added', 'age_of_show']
+
+negavite_check = df.loc[df['age_of_show'] < 0, col_to_check]
+
+## Handling this values
+df.loc[df['age_of_show'] < 0, 'age_of_show'] = np.nan
